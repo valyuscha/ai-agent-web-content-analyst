40dad89353bf331545437177997bbbfe4b85aba6
Valentyna Sydorovska <valentyna.sydorovska@tui.co.uk>
2026-04-13 20:46:23 +0200

    fix: Server Actions origin mismatch for production

--- web-analyst-web/frontend/next.config.js
+++ web-analyst-web/frontend/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     serverActions: {
-      allowedOrigins: ['178.104.87.224', '178.104.87.224:80'],
+      allowedOrigins: ['178-104-87-224.nip.io'],
     },
   },
 }
